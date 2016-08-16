be415a555e048821e18b48da5d4f4578980e7d99
Adrian <adrianlski@gmail.com>
2016-08-16 23:36:29 +0100

    files

---
+++ ConverterService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf.Logging;
+
+namespace Topshelf
+{
+    public class ConverterService
+    {
+        private FileSystemWatcher _watcher;
+        private static readonly LogWriter _logWriter = HostLogger.Get<ConverterService>();
+
+        public bool Start()
+        {
+            _watcher = new FileSystemWatcher(@"C:\temp\a", "*_in.txt");
+            _watcher.Created += FileCreated;
+            _watcher.IncludeSubdirectories = false;
+            _watcher.EnableRaisingEvents = true;
+
+            return true;
+        }
+
+        private void FileCreated(object sender, FileSystemEventArgs e)
+        {
+            _logWriter.InfoFormat("{0}", e.FullPath);
+            string content = File.ReadAllText(e.FullPath);
+            string upperContent = content.ToUpperInvariant();
+            var dir = Path.GetDirectoryName(e.FullPath);
+            var convertedFileName = Path.GetFileName(e.FullPath + ".converted");
+            var convertedPath = Path.Combine(dir, convertedFileName);
+
+            File.WriteAllText(convertedPath, upperContent);
+        }
+
+        public bool Stop()
+        {
+            _watcher.Dispose();
+            return true;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Topshelf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(serviceConfig =>
+            {
+                serviceConfig.Service<ConverterService>(serviceInstance =>
+                {
+                    serviceInstance.ConstructUsing(() => new ConverterService());
+                    serviceInstance.WhenStarted(execute => execute.Start());
+                    serviceInstance.WhenStopped(execute => execute.Stop());
+                });
+
+                serviceConfig.SetServiceName("AwesomeFileConverter");
+                serviceConfig.SetDisplayName("Awesome File Converter");
+                serviceConfig.SetDescription("Demo demo");
+                serviceConfig.UseNLog();
+
+                serviceConfig.StartAutomatically();
+            });
+        }
+    }
+}
